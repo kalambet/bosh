f516fe2c179456106aee2b31dcdb7fa44da5a9a5
Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>
2016-02-03 13:45:51 -0800

    new pipeline
    Signed-off-by: William Lindner <wlindner@pivotal.io>

--- ci/pipelines/os-images-new/tasks/build.sh
+++ ci/pipelines/os-images-new/tasks/build.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+exit 1
+
 set -eu
+
+
+WORKING_DIR=$PWD
 
 cd bosh-src
 source ci/tasks/utils.sh
-check_param BOSH_AWS_ACCESS_KEY_ID
-check_param BOSH_AWS_SECRET_ACCESS_KEY
 check_param OPERATING_SYSTEM_NAME
 check_param OPERATING_SYSTEM_VERSION
-check_param OS_IMAGE_S3_BUCKET_NAME
-check_param OS_IMAGE_S3_KEY
+
+OS_IMAGE_NAME=$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION-os-image
+OS_IMAGE_DIR=$WORKING_DIR/$OS_IMAGE_NAME/os-image.tgz
 
 sudo chown -R ubuntu .
 sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
     bundle install --local
-    bundle exec rake stemcell:build_os_image[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,/tmp/bosh-$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION-os-image.tgz]
-    bundle exec rake stemcell:upload_os_image[/tmp/bosh-$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION-os-image.tgz,$OS_IMAGE_S3_BUCKET_NAME,$OS_IMAGE_S3_KEY]
+    bundle exec rake stemcell:build_os_image[$OPERATING_SYSTEM_NAME,$OPERATING_SYSTEM_VERSION,$OS_IMAGE_PATH]
 SUDO
+
+#cd $WORKING_DIR
+#
+#OS_IMAGE_NAME=$OPERATING_SYSTEM_NAME-$OPERATING_SYSTEM_VERSION-os-image
+#mkdir -p $OS_IMAGE_NAME/
+#mv /tmp/os-image.tgz $OS_IMAGE_NAME/
